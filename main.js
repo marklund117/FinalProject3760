455e5007f8ef2959022750eaceef4f100d971994
Mark Lund <marklund117@gmail.com>
2023-11-19 23:02:23 -0700

    comments, ideas, etc

---
+++ main.js
@@ -0,0 +1,9 @@
+// this is the main JavaScript file for the Final Project
+
+// First, we need to establish an object structure for a media item
+
+// then we need an array to hold all media items
+
+// then a rendering function of some kind that re-displays everything
+
+// then specific functionality of buttons and the like
