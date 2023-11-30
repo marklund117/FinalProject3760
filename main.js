ef872c332dc20bbb862bec952dbdaac45affcb05
Mark Lund <marklund117@gmail.com>
2023-11-30 16:35:11 -0700

    data structure setup

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
 
 // First, we need to establish an object structure for a media item
 
+const mediaItem = {
+    itemTitle: '', // title as a string
+    itemCategory: '', // category (movie, book, game)
+    itemRating: 0, // 0-10 scale
+    itemCompletion: '', // completed, planned, dropped
+    itemFav: false, // simple bool
+    itemEditMode: false // toggle for edit mode
+}
+
 // then we need an array to hold all media items
+
+let itemArray = []
 
 // then a rendering function of some kind that re-displays everything
 
